6c2d93452e66d6afc5918ed74f5912169b13fe4b
Sofia Pereira <sosopereiraqzp@gmail.com>
2026-05-18 13:46:37 -0300

    ADD Projeto

---
+++ Trabalho-2/semaforo.ino
@@ -0,0 +1,42 @@
+#define led_vermelho 8
+#define led_amarelo  9
+#define led_verde 10
+
+
+
+void setup() {
+  pinMode (8, OUTPUT);
+  pinMode (9,OUTPUT);
+  pinMode (10, OUTPUT);
+
+}
+// Criando com Funções
+
+void fechado (int temp) {    
+  digitalWrite(8,HIGH);
+  digitalWrite (9,LOW);
+  digitalWrite (10,LOW);
+  delay(temp);
+}
+
+void aberto (int temp) {
+  digitalWrite(8,LOW);
+  digitalWrite (9,LOW);
+  digitalWrite (10,HIGH);
+  delay(temp);
+}
+
+void cuidado (int temp) {
+  digitalWrite(8,LOW);
+  digitalWrite (9,HIGH);
+  digitalWrite (10,LOW);
+  delay(temp);
+}
+
+
+void loop() {
+ fechado (6000);
+ aberto (8000);
+ cuidado (4000);
+
+}
